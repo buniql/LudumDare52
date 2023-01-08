84a29623aefa68b4d10d8908617963238750d5f8
Floke600 <florian1keller@web.de>
2023-01-08 18:18:53 +0100

    Read waves from CSV

--- LudumDare52/Assets/Scripts/Insect Spawner/InsectSpawner.cs
+++ LudumDare52/Assets/Scripts/Insect Spawner/InsectSpawner.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
-using UnityEngine.Serialization;
-using UnityEngine.Tilemaps;
 
 [System.Serializable]
 public enum InsectEnum
@@ -28,7 +26,7 @@ public class Pack
 public class Wave
 {
     public float timeBetweenPacks = 2f;
-    public List<Pack> packs;
+    public List<Pack> packs = new List<Pack>();
 
     public float TimeNeeded()
     {
@@ -41,7 +39,9 @@ public class Wave
 
 public class InsectSpawner : MonoBehaviour
 {
-    public List<Wave> waves;
+    public TextAsset waveData;
+
+    public List<Wave> waves = new List<Wave>();
 
     public GameObject ladybugPrefab;
     public GameObject antPrefab;
@@ -55,8 +55,57 @@ public class InsectSpawner : MonoBehaviour
 
     public float timeBetweenWaves = 15f;
 
+    private void ReadWaveDataFromCSV()
+    {
+        var csv = waveData;
+
+        var rows = csv.text.Split(new char[] { '\n' });
+
+        waves.Clear();
+
+        for (int i = 0; i < rows.Length; i++)
+        {
+            string[] wavedata = rows[i].Split(new char[] { ',' });
+
+            Wave wave = new Wave();
+            wave.timeBetweenPacks = float.Parse(wavedata[0], CultureInfo.InvariantCulture);
+
+            for (int j = 0; j < 5; j++)
+            {
+                int dataOffset = 1 + j*3;
+
+                Pack pack = new Pack();
+                switch (wavedata[dataOffset])
+                {
+                    case "Ladybug":     pack.type = InsectEnum.Ladybug; break;
+                    case "Ant":         pack.type = InsectEnum.Ant; break;
+                    case "Beetle":      pack.type = InsectEnum.Beetle; break;
+                    case "Caterpillar": pack.type = InsectEnum.Caterpillar; break;
+                    case "Spider":      pack.type = InsectEnum.Spider; break;
+                    case "Bee":         pack.type = InsectEnum.Bee; break;
+                    case "Snail":       pack.type = InsectEnum.Snail; break;
+                    case "-":           continue;
+                    default:            Debug.Log("Unknown insect type " + wavedata[dataOffset] + ", skipping"); break;
+                }
+
+                pack.amount = int.Parse(wavedata[dataOffset + 1]);
+                pack.timeBetweenSpawn = float.Parse(wavedata[dataOffset + 2], CultureInfo.InvariantCulture);
+
+                wave.packs.Add(pack);
+            }
+
+            waves.Add(wave);
+        }
+
+        for (int wave = 0; wave < waves.Count; wave++)
+        {
+            Debug.Log("wave " + wave + " starts with " + waves[wave].packs[0].amount + " " + waves[wave].packs[0].type + " (time between spawns: " + waves[wave].packs[0].timeBetweenSpawn);
+        }
+    }
+
     private void Start()
     {
+        ReadWaveDataFromCSV();
         StartCoroutine(SpawnEverything());
     }
 
